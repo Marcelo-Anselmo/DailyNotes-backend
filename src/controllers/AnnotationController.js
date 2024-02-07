d13f847cf8a27572e77d71b2466902f23e29e0a6
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2024-02-07 20:03:37 -0300

    feat:{PriorityController}

--- src/controllers/AnnotationController.js
+++ src/controllers/AnnotationController.js
@@ -8,12 +8,34 @@ module.exports = {
         return res.json(AnnotationsList);
     },
 
-    create(req, res) {
-        const { title, notes, priority } = request.body;
+    async create(req, res) {
+        const { title, notes, priority } = req.body;
 
-        console.log(title);
-        console.log(notes);
-        console.log(priority);
+        if(!notes || !title){
+            return res.status(400).json({ error: "Necessário um título/anotação!" });
+        }
+
+        const annotationsCreated = await Annotations.create({
+            title,
+            notes,
+            priority
+        });
+
+        return res.json(annotationsCreated);
+    },
+
+    async delete(req, res) {
+        const { id } = req.params;
+
+        const annotationDeleted = await Annotations.findOneAndDelete({
+            _id : id
+        });
+
+        if(annotationDeleted) {
+            return res.json(annotationDeleted);
+        }
+
+        return response.status(401).json({ error: "Não foi encontrado o registro para deletar" });
     }
 
 };
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const routes = express.Router();
 
 const AnnotationController = require('./controllers/AnnotationController');
+const PriorityController = require('./controllers/PriorityController');
 
 //Rota Annotations
 routes.post('/anottations', AnnotationController.create);
 routes.get('/anottations', AnnotationController.read);
+routes.delete('/anottations/:id', AnnotationController.delete);
+
+//Rota Priority
+routes.get('/priorities', PriorityController.read);
+routes.post('/priorities/:id', PriorityController.update);
 
 module.exports = routes;
